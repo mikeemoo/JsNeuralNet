2074ed45daaa6fec43a03e93ab06c2b98711ffb0
Mikee <mikeefranklin@gmail.com>
2012-10-09 14:26:09 +0100

    x

--- src/neuron.js
+++ src/neuron.js
@@ -1,16 +1,27 @@
-function Neuron(numWeights){
+function Neuron(numWeights) {
+
+    "use strict";
+
+    var i;
+
     this.weights = [];
-    for (var i = 0; i < numWeights; i++){
+
+    for (i = 0; i < numWeights; i += 1) {
         this.weights[i] = (Math.random() * 2) - 1;
     }
 }
 Neuron.prototype.output = function (inputs) {
-    var total = 0;
-    var threshold;
-    var numInputs = inputs.length;
-    for (var i=0; i < numInputs; i++){
+
+    "use strict";
+
+    var total = 0,
+        i,
+        threshold,
+        numInputs = inputs.length;
+
+    for (i = 0; i < numInputs; i += 1) {
         total += inputs[i] * this.weights[i];
     }
     threshold = this.weights[i];
     return 1 / (1 + Math.exp((-total) / threshold));
-}
+};
